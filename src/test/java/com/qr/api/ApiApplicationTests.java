9ae4b8dc4e919e9d26f9cfc33f15cd81c517d331
Roman Lezhnin <romalezhnin@gmail.com>
2022-05-17 22:26:20 +0300

    init

---
+++ src/test/java/com/qr/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
